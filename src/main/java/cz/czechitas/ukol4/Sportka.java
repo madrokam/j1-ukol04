c42796d61cf8b9a9d9fc1a40cd6bb9214b69976e
XMADK006 <xmadk006@studenti.czu.cz>
2023-03-26 11:04:44 +0200

    odevzdej ukol c.4

--- src/main/java/cz/czechitas/ukol4/Sportka.java
+++ src/main/java/cz/czechitas/ukol4/Sportka.java
@@ -3,6 +3,8 @@ package cz.czechitas.ukol4;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.*;
+import java.util.stream.IntStream;
 
 /**
  * Třída pro losování Sportky.
@@ -12,6 +14,11 @@ public class Sportka {
 
   public Sportka() {
     //TODO naplnit osudí čísly 1 až 49.
+
+
+for(int i=0; i<=49;i++){this.osudi.add(i);}
+
+
   }
 
   /**
@@ -20,7 +27,9 @@ public class Sportka {
    * @see Collections#shuffle(List)
    */
   public void zamichat() {
+
     //TODO zamíchat osudí
+    Collections.shuffle(this.osudi);
   }
 
   /**
@@ -32,7 +41,23 @@ public class Sportka {
    */
   public List<Integer> vylosovanaCisla() {
     //TODO Vrátit seznam prvních 6 čísel z osudí.
-    return null;
+//alternativni reseni bez subListu
+    /*int prvni=this.osudi.get(0);
+    int druhy=this.osudi.get(1);
+    int treti=this.osudi.get(2);
+    int ctvrty=this.osudi.get(3);
+    int paty=this.osudi.get(4);
+    int sesty=this.osudi.get(5);
+    List<Integer> list=new ArrayList<>();
+    list.add(prvni);
+    list.add(druhy);
+    list.add(treti);
+    list.add(ctvrty);
+    list.add(paty);
+    list.add(sesty);*/
+
+
+    return this.osudi.subList(0,6);
   }
 
   /**
@@ -42,6 +67,6 @@ public class Sportka {
    */
   public Integer dodatkoveCislo() {
     //TODO Vrátit sedmé číslo z osudí.
-    return null;
+    return this.osudi.get(6);
   }
 }
